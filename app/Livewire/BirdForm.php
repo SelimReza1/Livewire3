7ea65578500df5d723dc42364731da690f28248d
SelimReza1 <selimreza9920@gmail.com>
2025-05-23 00:04:55 +0600

    data shor from database and validation

--- app/Livewire/BirdForm.php
+++ app/Livewire/BirdForm.php
@@ -4,14 +4,25 @@ namespace App\Livewire;
 
 use App\Models\Entry;
 use Livewire\Component;
+use Livewire\Attributes\validate;
 
 class BirdForm extends Component
 {
+
+    #[validate('required|integer')]
     public int $count;
+    
+    #[validate('required|string')]
     public string $note;
 
     public function submit()
     {
+        // $this->validate([
+        //     'count' => 'required|integer',
+        //     'note' => 'required|string',
+        // ]);
+        $this->validate();
+
         Entry::create([
             'count' => $this->pull('count'),
             'note' => $this->pull('note')
@@ -22,6 +33,8 @@ class BirdForm extends Component
 
     public function render()
     {
-        return view('livewire.bird-form');
+        return view('livewire.bird-form', [
+            'entries' => Entry::all(),
+        ]);
     }
 }
--- resources/views/livewire/bird-form.blade.php
+++ resources/views/livewire/bird-form.blade.php
@@ -12,4 +12,19 @@
         <br>
         <button>Add a new bird entry</button>
     </form>
+
+    @if ($errors->any())
+        <div>
+            @foreach ($errors->all() as $error)
+                <div>{{$error}}</div>
+            @endforeach
+        </div>
+    @endif
+
+    <h1> All Bird Entries</h1>
+
+    @foreach ($entries as $entry)
+        <div>{{ $entry->count}}</div>
+        <div>{{ $entry->note}}</div>
+    @endforeach
 </div>
